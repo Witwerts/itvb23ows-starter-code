3b4b7ece5d1454efe18148635c08dfac886dd0cd
Witwerts <albertwitwerts@msn.com>
2024-03-28 14:54:51 +0100

    Fix bug 5
    - Fixed undo functionality by updating the state by the previous move (instead of the current) and resets the game if there is only 1 move set.

--- main/move.php
+++ main/move.php
@@ -60,6 +60,7 @@ else {
         $stmt = $db->prepare('insert into moves (game_id, type, move_from, move_to, previous_id, state) values (?, "move", ?, ?, ?, ?)');
         $stmt->bind_param('issis', $_SESSION['game_id'], $from, $to, $_SESSION['last_move'], $state);
         $stmt->execute();
+
         $_SESSION['last_move'] = $db->insert_id;
 
         if(empty($board[$from]))
--- main/undo.php
+++ main/undo.php
@@ -3,11 +3,33 @@
 session_start();
 
 $db = include 'database.php';
-$stmt = $db->prepare('SELECT * FROM moves WHERE id = '.$_SESSION['last_move']);
-$stmt->execute();
-$result = $stmt->get_result()->fetch_array();
-$_SESSION['last_move'] = $result[5];
-set_state($result[6]);
+
+if(isset($_SESSION['last_move'])){
+    $stmt = $db->prepare('SELECT * FROM moves WHERE id = '.$_SESSION['last_move']);
+    $stmt->execute();
+    $currMove = $stmt->get_result()->fetch_array();
+
+    if(!empty($currMove)){
+        $db->prepare('DELETE FROM moves where id = '.$currMove["id"])->execute();
+        $prevId = $currMove["previous_id"];
+
+        if(!is_null($prevId)){
+            $stmt = $db->prepare('SELECT * FROM moves WHERE id = '.$prevId);
+            $stmt->execute();
+            $oldMove = $stmt->get_result()->fetch_array();
+    
+            $_SESSION['last_move'] = $prevId;
+
+            if(!empty($oldMove))
+                set_state($oldMove["state"]);
+        }
+        else {
+            header('Location: restart.php');
+            return;
+        }
+    }
+}
+
 header('Location: index.php');
 
 ?>
