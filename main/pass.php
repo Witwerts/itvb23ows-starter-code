0544acae42a8ecf4b70e33e085521958c565d7a6
Witwerts <albertwitwerts@msn.com>
2024-03-29 14:03:17 +0100

    Code optimalization: changed to switchTurn in Pass

--- main/pass.php
+++ main/pass.php
@@ -14,7 +14,7 @@ if(isset($board) && !canMove($board, $player)){
     $stmt->bind_param('iis', $_SESSION['game_id'], $_SESSION['last_move'], $state);
     $stmt->execute();
     $_SESSION['last_move'] = $db->insert_id;
-    $_SESSION['player'] = 1 - $_SESSION['player'];
+    switchTurn();
 }
 
 header('Location: index.php');
